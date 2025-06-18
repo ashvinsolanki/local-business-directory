7bfe3297a8ab7e743041a6fa008a17f746bddb8d
ashvinsolanki <ashvinsolanki42@gmail.com>
2025-06-18 16:54:35 +0530

    project setup from local

---
+++ database/migrations/2019_09_16_000006_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+
+            $table->string('address')->nullable();
+
+            $table->longText('description')->nullable();
+
+            $table->string('website');
+
+            $table->string('phone');
+
+            $table->string('email');
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+}
